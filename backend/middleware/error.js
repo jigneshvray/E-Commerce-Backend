32d33161d5ae596532675a18c48e18c927a67dce
jigneshvray <jigneshvray@gmail.com>
2022-02-28 14:29:58 +0530

    commit from Jignesh

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -5,7 +5,7 @@ module.exports = (err,req,res,next) =>{
 
     err.statusCode = err.statusCode || 500;
     err.message = err.message || "Internal Server Error";
-
+    
 
 // Wrong Mongodb Id error
 if (err.name === "CastError") {
