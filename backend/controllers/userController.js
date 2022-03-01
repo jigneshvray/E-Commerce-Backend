c98226496e7326d745da5a7140680f8536a498e6
jigneshvray <jigneshvray@gmail.com>
2022-03-02 01:36:18 +0530

    login error fix by jigu

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,3 +22,37 @@ exports.registerUser = catchAsyncErrors(async(req,res,next)=>{
         token,
     });
 });
+
+// Login User
+
+exports.loginUser = catchAsyncErrors (async (req,res,next) => {
+
+    const {email,password} = req.body;
+
+// checking if user has given password and email both 
+
+    if(!email || !password){
+        return next(new ErrorHandler("Please Enter Email & Password",400))
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if(!user){
+        return next(new ErrorHandler("Invalid Email or Password",401));
+    }
+
+    const isPasswordMatched = await user.comparePassword(password);
+
+    if(!isPasswordMatched){
+        return next(new ErrorHandler("Invalid Email or Password",401));
+    }
+
+    const token = user.getJWTToken();
+
+    res.status(200).json({
+        success:true,
+        token,
+    });
+
+
+});
