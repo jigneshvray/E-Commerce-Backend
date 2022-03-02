1bf8219d17fc35d6e2364767222806fdf5cf3a4a
jigneshvray <jigneshvray@gmail.com>
2022-03-03 04:27:28 +0530

    Jignesh & Tushar commit

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const { getAllProducts,createProduct, updateProducts, deleteProduct, getProductDetails } = require("../controllers/productController");
+const { 
+        getAllProducts,
+        createProduct,
+        updateProducts,
+        deleteProduct,
+        getProductDetails 
+    } = require("../controllers/productController");
+const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 
 const router=express.Router();
 
 router.route("/products").get(getAllProducts);
 
-router.route("/product/new").post(createProduct);
+router
+    .route("/product/new")
+    .post(isAuthenticatedUser,authorizeRoles("admin"), createProduct);
 
-router.route("/product/:id").put(updateProducts).delete(deleteProduct).get(getProductDetails)
+router
+    .route("/product/:id")
+    .put(isAuthenticatedUser,authorizeRoles("admin"),updateProducts)
+    .delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct)
+    .get(getProductDetails)
 
 
 module.exports = router;
